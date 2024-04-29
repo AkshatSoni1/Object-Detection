714183d2ce0015bc43c1813f4a15f7d3988e9ed5
AkshatSoni1 <sakshat551@gmail.com>
2024-04-29 21:57:10 +0530

    second  commit

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
       <h1 
-        className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
+        className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center p-2">
         Object Detection Software
       </h1>
       <ObjectDetection />
--- utils/render-predictions.js
+++ utils/render-predictions.js
@@ -1,41 +1,43 @@
-import { throttle } from "lodash";
+import {throttle} from "lodash";
 
 export const renderPredictions = (predictions, ctx) => {
-    ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    const font = "16px sans-serif"
-    ctx.font = font;
-    ctx.textBaseline = "top"
+  // Fonts
+  const font = "16px sans-serif";
+  ctx.font = font;
+  ctx.textBaseline = "top";
 
-    predictions.forEach((prediction) => {
-        const [x, y, width, height] = prediction["bbox"];
+  predictions.forEach((prediction) => {
+    const [x, y, width, height] = prediction["bbox"];
 
-        const isPerson = prediction.class === "person";
+    const isPerson = prediction.class === "person";
 
-        ctx.strokeStyle = isPerson ? "#FF0000" : "#00FFFF";
-        ctx.lineWidth = 4;
-        ctx.strokeRect(x, y, width, height);
+    // bounding box
+    ctx.strokeStyle = isPerson ? "#FF0000" : "#00FFFF";
+    ctx.lineWidth = 4;
+    ctx.strokeRect(x, y, width, height);
 
-        // fill color
-        ctx.fillStyle = `rgba(255, 0, 0, ${isPerson ? 0.2 : 0})`;
-        ctx.fillRect(x, y, width, height);
+    // fill the color
+    ctx.fillStyle = `rgba(255, 0, 0, ${isPerson ? 0.2 : 0})`; // Set the fill color to red
+    ctx.fillRect(x, y, width, height);
 
-        // Draw label background
-        ctx.fillStyle = isPerson ? "#FF0000" : "00FFFF";
-        const textWidth = ctx.measureText(prediction.class).width
-        const textHeight = ctx.parseInt(font, 10) //base 10
-        ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    // Draw the label background.
+    ctx.fillStyle = isPerson ? "#FF0000" : "#00FFFF";
+    const textWidth = ctx.measureText(prediction.class).width;
+    const textHeight = parseInt(font, 10); // base 10
+    ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
 
-        ctx.fillStyle = "#000000"
-        ctx.fillText(prediction.class, x, y);
+    ctx.fillStyle = "#000000";
+    ctx.fillText(prediction.class, x, y);
 
-        // if(isPerson){
-        //     playAudio();
-        // }
-    });
-}
+    // if (isPerson) {
+    //   playAudio();
+    // }
+  });
+};
 
 // const playAudio = throttle(() => {
-//     const audio = new Audio("");
-//     audio.play();
+//   const audio = new Audio("/pols-aagyi-pols.mp3");
+//   audio.play();
 // }, 2000);
